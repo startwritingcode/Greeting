f0fce23aab83b02f62fcf5f500ac6e8687148e63
Derek Jensen <derek@startwritingcode.com>
2018-10-24 00:05:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            string greeting = "I'm glad you are learning C# today, ";
+            string fullGreeting = greeting + userName;
+            Console.WriteLine(fullGreeting);
+        }
+    }
+}
